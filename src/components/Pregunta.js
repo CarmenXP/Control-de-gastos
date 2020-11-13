dd91908674cb8b8ae52fe15018a8cab7a4169a22
CarmenXP <xocuaposadas.carmen@gmail.com>
2020-11-13 06:16:38 -0600

    Primeros componentes

---
+++ src/components/Pregunta.js
@@ -0,0 +1,55 @@
+import Error from './Error';
+const { Fragment } = require("react");
+const {useState} = require("react");
+
+
+export default function Pregunta() {
+
+//definiendo el state para presupuesto
+const [cantidad, guardadCantidad]= useState(0);
+// state paa el error
+const [error, guardarError] = useState(false);
+
+//leyendo la cantidad en el input
+const definirPresupuesto = e =>{
+    guardadCantidad(parseInt(e.target.value, 10))
+}
+// evento onSubmit
+const agregarPresupuesto = e=>{
+    e.preventDefault();
+    //validación
+    if(cantidad <1 || isNaN(cantidad)){
+        guardarError(true);
+        return;
+    }
+    //si pasa validación
+        guardarError(false);
+}
+
+
+    return (
+        
+
+        <Fragment>
+            <h2> Coloca tu presupuesto</h2>
+            {error ? <Error mensaje ="La cantidad introducida es incorrecta"/>: null}
+            <form
+                onSubmit={agregarPresupuesto}
+            >
+                <input
+                type ="number"
+                className ="u-full-width"
+                placeholder="Coloca tu presupuesto"
+                onChange={definirPresupuesto}
+                />
+                <input 
+                type= "submit"
+                className="button-primary u-full-width"
+                value="Definir Presupuesto"
+                />
+            </form>
+        </Fragment>
+    )
+}
+
+
